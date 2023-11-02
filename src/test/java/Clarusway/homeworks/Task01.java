9a74c19311cee6b491909487771d7f2d5e0a7d9e
ZeynepA <zeyneppaytop@gmail.com>
2023-11-02 23:33:35 +0300

    TestNGProject 02.11.2023

---
+++ src/test/java/Clarusway/homeworks/Task01.java
@@ -0,0 +1,13 @@
+package Clarusway.homeworks;
+
+public class Task01 {
+    /*
+Create tests that depend on each other
+Create beforeClass and set up settings.
+By creating interdependent tests;
+First go to Facebook.
+Then go to Google depending on Facebook,
+Then go to Amazon depending on Google
+Close the driver.
+ */
+}
