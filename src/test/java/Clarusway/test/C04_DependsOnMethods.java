7b505274868ff5dee7963d643c7dcec6c873d07c
ZeynepA <zeyneppaytop@gmail.com>
2023-11-02 22:52:04 +0300

    TestNGProject 02.11.2023

---
+++ src/test/java/Clarusway/test/C04_DependsOnMethods.java
@@ -0,0 +1,34 @@
+package Clarusway.test;
+
+import org.testng.annotations.Test;
+
+public class C04_DependsOnMethods {
+    // DependsOnMethods test method'larinin calisma siralamasina karismaz
+    // Sadece bagli olan test, baglandigi testin sonucuna bakar
+    // baglandigi test PASSED olmazsa, baglanan test hic calismaz(ignore)
+
+
+    @Test
+    void startCar(){
+        System.out.println("araba calistir"); //bu test bağımsız
+    }
+    @Test (dependsOnMethods = {"startCar"})//bu testin calismasi startCar testinin hatasiz calismasina bagli
+    void driveCar(){
+        System.out.println("araba sur");
+    }
+    @Test(dependsOnMethods = {"parkCar"})//bu testin calismasi parkCar testinin hatasiz calismasina bagli
+    void stopCar(){
+        System.out.println("araba durdur");
+    }
+    @Test(dependsOnMethods = {"driveCar"})//bu testin çalışması driveCar testinin hatasız çalışmasına bağlı
+    void parkCar(){
+        System.out.println("araba park halinde");
+    }
+    /*
+    araba calistir
+    araba sur
+    araba park et
+    araba durdur
+     */
+}
+
