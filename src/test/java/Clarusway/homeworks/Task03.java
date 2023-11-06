3fe07dc178e2e31271df72c63d2ee36a24eab6c1
ZeynepA <zeyneppaytop@gmail.com>
2023-11-07 00:33:41 +0300

    TestNGProject 07.11.2023

--- src/test/java/Clarusway/homeworks/Task03.java
+++ src/test/java/Clarusway/homeworks/Task03.java
@@ -1,6 +1,9 @@
 package Clarusway.homeworks;
 
 import Clarusway.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Task03 extends TestBase {
@@ -12,12 +15,22 @@ public class Task03 extends TestBase {
 //Using the Search function do it with Data Provider for Mac, iPad and Samsung.
      */
 
-    @Test
-    public void test(){
+    @Test(dataProvider = "aranacakKelime")
+    public void test(String keywords){
         driver.get("http://opencart.abstracta.us/index.php?route=account/login");
 
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("clarusway@gmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("123456789");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
 
+        driver.findElement(By.xpath("//input[@name='search']")).sendKeys(keywords + Keys.ENTER);
 
+    }
 
+    @DataProvider(name = "aranacakKelime")
+    public Object[][] getData2(){
+
+        Object[][] data ={{"Mac"},{"iPad"},{"Samsung"}};
+        return data;
     }
 }
