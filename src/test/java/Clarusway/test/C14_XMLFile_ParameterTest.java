4603520de6a3adc13ca8326d79a8b1446f5fc215
ZeynepA <zeyneppaytop@gmail.com>
2023-11-06 23:08:03 +0300

    TestNGProject 06.11.2023

--- src/test/java/Clarusway/test/C14_XMLFile_ParameterTest.java
+++ src/test/java/Clarusway/test/C14_XMLFile_ParameterTest.java
@@ -6,28 +6,34 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.v85.animation.model.KeyframeStyle;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class C14_XMLFile_ParameterTest extends TestBase {
-
     //Go to URL: https://www.amazon.com
     //Search words: Java, Selenium
     //Assert get text result test that the result text contains the searched Word.
     //Run tests from XML file.
 
+        /*    @Test
+            public void test1(){
+                driver.navigate().to("https://www.amazon.com");
+                WebElement searchArea = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+                searchArea.sendKeys("Java"+ Keys.ENTER);
+                WebElement result = driver.findElement(By.xpath("//span[@class='a-color-state a-text-bold']"));
+                Assert.assertTrue(result.getText().toLowerCase().contains("java"));
+                driver.navigate().back();
+                searchArea.sendKeys("Selenium"+ Keys.ENTER);
+                Assert.assertTrue(result.getText().toLowerCase().contains("selenium"));
+            }*/
+
     @Test
-    public void test(){
-        driver.get("http://www.amazon.com");
-
+    @Parameters("aranacakKelime")
+    public void test2(String keyword){
+        driver.get("https://www.amazon.com");
         WebElement searchArea = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
-        searchArea.sendKeys("Java"+ Keys.ENTER);
-
+        searchArea.sendKeys(keyword+ Keys.ENTER);
         WebElement result = driver.findElement(By.xpath("//span[@class='a-color-state a-text-bold']"));
-
-        Assert.assertTrue(result.getText().contains("java"));
-
-
-
+        Assert.assertTrue(result.getText().toLowerCase().contains(keyword.toLowerCase()));
     }
-
 }
