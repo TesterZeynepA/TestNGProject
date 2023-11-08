f0a5f725c3526ca5fc84472b6a30b0b36a5cd81d
ZeynepA <zeyneppaytop@gmail.com>
2023-11-08 22:16:56 +0300

    TestNGProject 08.11.2023

---
+++ src/test/java/Clarusway/smokeTest/NegativeLogin.java
@@ -0,0 +1,37 @@
+package Clarusway.smokeTest;
+
+import Clarusway.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class NegativeLogin extends TestBase {
+//Test Case2: Negative Username Test
+    //Open page https://practicetestautomation.com/practice-test-login/
+    //Type username incorrectUser into Username field.
+    //Type password Password123 into Password field.
+    //Puch Submit button.
+    //Verify error message is displayed.
+    //Verify error message text is Your username is invalid!
+
+    @Test
+    public void negativeUserNameTestSoft(){
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("incorrectUser");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Password123");
+
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
+
+        SoftAssert softAssert = new SoftAssert();
+
+        WebElement errorMessage= driver.findElement(By.xpath("//div[@id='error']"));
+
+        softAssert.assertTrue(errorMessage.isDisplayed());
+
+        softAssert.assertEquals("Your username is invalid!", errorMessage.getText());
+
+        softAssert.assertAll();
+
+    }
+}
